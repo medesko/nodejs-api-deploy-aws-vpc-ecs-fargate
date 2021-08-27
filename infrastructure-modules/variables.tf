255e5d951dd66dbaa3550e0d2d4441dc207f9420
Mohamed KEITA <medesko@gmail.com>
2021-08-28 00:09:33 +0200

    set var app name and env

--- infrastructure-modules/variables.tf
+++ infrastructure-modules/variables.tf
@@ -25,3 +25,23 @@ variable "app_port" {
   description = "Application port"
   type = number
 }
+
+variable "app_image" {
+  type = string 
+  description = "Container image to be used for application in task definition file"
+}
+
+variable "availability_zones" {
+  type  = list(string)
+  description = "List of availability zones for the selected region"
+}
+
+variable "app_count" {
+  type = string 
+  description = "The number of instances of the task definition to place and keep running."
+}
+
+variable "main_pvt_route_table_id" {
+  type        = string
+  description = "Main route table id"
+}
