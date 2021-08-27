cc272688a40dabbce39e8b874f207da8825f0cf5
Mohamed KEITA <medesko@gmail.com>
2021-08-28 00:19:24 +0200

    set var cidr

--- infrastructure-modules/vpc/vars.tf
+++ infrastructure-modules/vpc/vars.tf
@@ -7,25 +7,26 @@ variable "number_of_private_subnets" {
   default = 1
   description = "The number of private subnets in a VPC."
 }
-
 variable "private_subnet_tag_name" {
   type        = string
   description = "Name tag for the private subnet"
 }
-
 variable "route_table_tag_name" {
   type        = string
   default     = "main"
   description = "Route table description"
 }
-
 variable "main_pvt_route_table_id" {
   type        = string
   description = "Main route table id"
 }
-
 variable "vpc_cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
   description = "CIDR block range for vpc"
+}
+variable "private_subnet_cidr_blocks" {
+  type        = list(string)
+  default     = ["10.0.0.0/24", "10.0.4.0/24"]
+  description = "CIDR block range for the private subnets"
 }
