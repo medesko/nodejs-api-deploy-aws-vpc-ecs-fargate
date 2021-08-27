4f0ca868b7c3e8dd7ab12d314e343712205f838b
Mohamed KEITA <medesko@gmail.com>
2021-08-28 00:40:05 +0200

    add ouput

--- infrastructure-modules/vpc/vpc.tf
+++ infrastructure-modules/vpc/vpc.tf
@@ -44,3 +44,48 @@ resource "aws_security_group" "lb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# Traffic to the ECS Cluster should only come from the ALB
+# or AWS services through an AWS PrivateLink
+resource "aws_security_group" "ecs_tasks" {
+  name        = "${var.security_group_ecs_tasks_name}-${var.environment}"
+  description = var.security_group_ecs_tasks_description
+  vpc_id      = "${aws_vpc.custom_vpc.id}"
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 443
+    to_port         = 443
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    prefix_list_ids = [
+      aws_vpc_endpoint.s3.prefix_list_id
+    ]
+  }
+
+  egress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks = [var.vpc_cidr_block]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
